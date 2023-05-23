af73f6460c5838e15c5d5149998c57c46fd5ef6a
Munana Clinton <munanaclinton@gmail.com>
2023-05-23 22:53:12 +0200

    Add display list file render.js

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,1 +1,18 @@
+import {getMovies, movies} from './api.js'
+
+const section = document.querySelector("section");
 let genreArray = ["dance", "girls"];
+
+export const render = async () => {
+    await Promise.all(genreArray.map((genre) => getMovies(genre)));
+    movies.forEach((obj, key) => {
+      let value = 0;
+      const container = document.createElement("div");
+      container.className = "container";
+      container.setAttribute("id", key);
+      container.innerHTML = `<img class='image' src=${obj.show.image.medium}><h3>${obj.show.name}</h3><div class='reactions'><div><i id=${key} class='fa fa-heart'></i><span class='likes'>${value}</span></div><div><i class='fa fa-comments'></i><span class='likes'></span></div></div><div id=${key} class='comments'>Comments</div>`;
+      section.appendChild(container);
+    });
+  };
+  
+  export default render
