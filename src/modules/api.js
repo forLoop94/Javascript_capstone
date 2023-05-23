36a535d1fe79ddbb52c9155411f492647743ff34
Munana Clinton <munanaclinton@gmail.com>
2023-05-23 23:08:42 +0200

    Add changes from linter

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,14 +1,10 @@
-
-export let movies = [];
+export const movies = [];
 export const getMovies = async (genre) => {
-    try {
-      const res = await fetch(`https://api.tvmaze.com/search/shows?q=${genre}`);
-      const data = await res.json();
-      movies.push(...data);
-    } catch (error) {
-      return error;
-    }
+  try {
+    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${genre}`);
+    const data = await res.json();
+    return movies.push(...data);
+  } catch (error) {
+    return error;
   }
-
-  
-  
+};
