6f67ceb0b817b84bb627c44f9704c7fa2aa393f4
Munana Clinton <munanaclinton@gmail.com>
2023-05-24 20:42:43 +0200

    Add change to Api.js file, index.js and likes.js file

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-cycle */
 import { getMovies, movies } from './api.js';
 import popupHandler from './popupHandler.js';
+import { likes, showLike } from './likes.js';
 
 export const section = document.querySelector('section');
 const genreArray = ['dance', 'girls'];
@@ -12,9 +13,14 @@ export const render = async () => {
     const container = document.createElement('div');
     container.className = 'container';
     container.setAttribute('id', key);
-    container.innerHTML = `<img class='image' src=${obj.show.image.medium} alt="${obj.show.name}"><h3>${obj.show.name}</h3><div class='reactions'><div><i id=${key} class='fa fa-heart'></i><span class='likes'>${value}</span></div><div><i class='fa fa-comments'></i><span class='likes'></span></div></div><div id=${key} class='comments'>Comments</div>`;
+    container.innerHTML = `<img class='image' src=${obj.show.image.medium} alt="${obj.show.name}"><h3>${obj.show.name}</h3><div class='reactions'><div><i id=${key} class='fa fa-heart'></i><span class='likes' id=${key}>${value}</span></div><div><i class='fa fa-comments'></i><span class='comments'></span></div></div><div id=${key} class='comments'>Comments</div>`;
+    showLike();
     section.appendChild(container);
 
+    const hearts = document.querySelectorAll('.fa-heart');
+    hearts.forEach((heart) => {
+      heart.addEventListener('click', likes);
+    });
     const comments = document.querySelectorAll('.comments');
     comments.forEach((comment) => comment.addEventListener('click', popupHandler));
   });
