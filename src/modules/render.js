42d7c11edab496b46df6e9762e81363b0da708b9
forLoop94 <charlesuwem94@gmail.com>
2023-05-25 10:10:45 +0100

    Change ids from keys to movie ids

--- src/modules/render.js
+++ src/modules/render.js
@@ -11,8 +11,8 @@ export const render = async () => {
     const value = 0;
     const container = document.createElement('div');
     container.className = 'container';
-    container.setAttribute('id', key);
-    container.innerHTML = `<img class='image' src=${obj.show.image.medium} alt="${obj.show.name}"><h3>${obj.show.name}</h3><div class='reactions'><div><i id=${key} class='fa fa-heart'></i><span class='likes'>${value}</span></div><div><i class='fa fa-comments'></i><span class='likes'></span></div></div><div id=${key} class='comments'>Comments</div>`;
+    container.setAttribute('id', obj.show.id);
+    container.innerHTML = `<img class='image' src=${obj.show.image.medium} alt="${obj.show.name}"><h3>${obj.show.name}</h3><div class='reactions'><div><i id=${obj.show.id} class='fa fa-heart'></i><span class='likes'>${value}</span></div><div><i class='fa fa-comments'></i><span class='likes'></span></div></div><div id=${obj.show.id} class='comments'>Comments</div>`;
     section.appendChild(container);
 
     const comments = document.querySelectorAll('.comments');
