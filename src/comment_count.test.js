8ab84dbb0404cce380012ca068650870a619244f
forLoop94 <charlesuwem94@gmail.com>
2023-05-26 15:50:08 +0100

    Add test file for comment display function

---
+++ src/comment_count.test.js
@@ -0,0 +1,16 @@
+const commentCount = require('../_mocks_/comment_count.js');
+
+describe('Comment_count', () => {
+  test('commentcountWhenCommentArrayIsEmpty', () => {
+    expect(commentCount([])).toBe(0);
+  });
+  test('commentcountWhenCommentArrayIsnotEmpty', () => {
+    expect(commentCount(['comment1', 'comment2', 'comment3'])).toBe(3);
+  });
+  test('commentcountWhenCommentArrayIsnotEmpty', () => {
+    expect(commentCount([
+      { comment: 'So scary', creation_date: '2023-05-24', username: 'Clinton' },
+      { comment: 'Cool movie', creation_date: '2023-05-24', username: 'Charles' }
+    ])).toBe(2);
+  });
+});
